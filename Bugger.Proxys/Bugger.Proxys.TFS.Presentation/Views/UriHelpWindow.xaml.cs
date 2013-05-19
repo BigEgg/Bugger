42072f7674915d59e1c30b944b92874d3d9f0e9e
BigEgg_cp <SND\BigEgg_cp>
2013-05-19 09:31:00 -0700

    Add the Presentation project for Proxy.TFS project, and create the view of IUriHelpView.

---
+++ Bugger.Proxys/Bugger.Proxys.TFS.Presentation/Views/UriHelpWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Bugger.Proxys.TFS.Views;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace Bugger.Proxys.TFS.Presentation
+{
+    [Export(typeof(IUriHelpView)), PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class UriHelpWindow : Window, IUriHelpView
+    {
+        public UriHelpWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        public void ShowDialog(object owner)
+        {
+            Owner = owner as Window;
+            ShowDialog();
+        }
+    }
+}
