6a5f8b3952282d13c5b0c8cdaee4c07c20fa9729
BigEgg_cp <SND\BigEgg_cp>
2013-11-25 00:43:26 -0800

    Complete the TFSSetting View.

--- Bugger.Proxies/Bugger.Proxy.TFS.Presentation/DesignData/SampleTFSSettingViewModel.cs
+++ Bugger.Proxies/Bugger.Proxy.TFS.Presentation/DesignData/SampleTFSSettingViewModel.cs
@@ -4,6 +4,7 @@ using Bugger.Proxy.TFS.Presentation.Properties;
 using Bugger.Proxy.TFS.ViewModels;
 using Bugger.Proxy.TFS.Views;
 using System;
+using System.Linq;
 
 namespace Bugger.Proxy.TFS.Presentation.DesignData
 {
@@ -17,13 +18,28 @@ namespace Bugger.Proxy.TFS.Presentation.DesignData
             this.BugFilterValue = "Bugs";
             this.UserName = "BigEgg";
             this.Password = "Password";
+
+            this.PriorityValues.Add(new CheckString("High"));
+            this.PriorityValues.Add(new CheckString("Medium"));
+            this.PriorityValues.Add(new CheckString("Low"));
+            var values = this.PriorityValues.Where(x => x.Name == "High" || x.Name == "Medium");
+            foreach (var value in values)
+            {
+                value.IsChecked = true;
+            }
             this.PriorityRed = "High;Medium";
 
             var field = new TFSField("Work Item Type");
             field.AllowedValues.Add("Work Item");
             field.AllowedValues.Add("Bugs");
 
+            var idField = new TFSField("ID");
+
+            this.TFSFields.Add(idField);
             this.TFSFields.Add(field);
+
+            this.PropertyMappingCollection["ID"] = "ID";
+
             this.BugFilterFields.Add(field);
             this.ProgressType = ProgressTypes.SuccessWithError;
             this.ProgressValue = 100;
