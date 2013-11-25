0f7e1478fae09f9b9f4dcf61f70af6a4160b3938
BigEgg_cp <SND\BigEgg_cp>
2013-11-24 23:24:24 -0800

    Create the SampleUriHelpViewModel class for UriHelpWindow.

---
+++ Bugger.Proxies/Bugger.Proxy.TFS.Presentation/DesignData/SampleUriHelpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BigEgg.Framework.Presentation;
+using Bugger.Proxy.TFS.ViewModels;
+using Bugger.Proxy.TFS.Views;
+
+namespace Bugger.Proxy.TFS.Presentation.DesignData
+{
+    public class SampleUriHelpViewModel : UriHelpViewModel
+    {
+        public SampleUriHelpViewModel()
+            : base(new MockUriHelpViewModel())
+        {
+            this.ServerName = "https://tfs.codeplex.com:443/tfs/TFS12";
+        }
+
+
+        private class MockUriHelpViewModel : MockDialogView, IUriHelpView
+        {
+        }
+    }
+}
