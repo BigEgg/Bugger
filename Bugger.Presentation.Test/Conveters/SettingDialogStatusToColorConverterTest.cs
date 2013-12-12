391f3f258a00a0abd6bff9a59d108938b731408b
BigEgg_cp <SND\BigEgg_cp>
2013-12-12 00:42:55 -0800

    Add the unit test for the converters.

---
+++ Bugger.Presentation.Test/Conveters/SettingDialogStatusToColorConverterTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using Bugger.Applications.Models;
+using Bugger.Presentation.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bugger.Presentation.Test.Conveters
+{
+    [TestClass]
+    public class SettingDialogStatusToColorConverterTest
+    {
+        [TestMethod]
+        public void ConvertTest()
+        {
+            var converter = SettingDialogStatusToColorConverter.Default;
+
+            var result = converter.Convert(SettingDialogStatus.InitiatingProxyFailed, null, null, null);
+            Assert.AreEqual(Brushes.Red, result);
+
+            result = converter.Convert(SettingDialogStatus.InitiatingProxy, null, null, null);
+            Assert.AreEqual(Brushes.Green, result);
+
+            result = converter.Convert(SettingDialogStatus.NotWorking, null, null, null);
+            Assert.AreEqual(Brushes.Green, result);
+
+            result = converter.Convert(SettingDialogStatus.ProxyValid, null, null, null);
+            Assert.AreEqual(Brushes.Green, result);
+
+            result = converter.Convert(SettingDialogStatus.ValidatingProxySettings, null, null, null);
+            Assert.AreEqual(Brushes.Green, result);
+
+            result = converter.Convert(SettingDialogStatus.ProxyBusy, null, null, null);
+            Assert.AreEqual(Brushes.Orange, result);
+
+            result = converter.Convert(SettingDialogStatus.ProxyCannotConnect, null, null, null);
+            Assert.AreEqual(Brushes.Orange, result);
+
+            result = converter.Convert(SettingDialogStatus.ProxyUnvalid, null, null, null);
+            Assert.AreEqual(Brushes.Orange, result);
+        }
+    }
+}
---
+++ Bugger.Presentation.Test/Conveters/SettingDialogStatusToIndeterminateConverterTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bugger.Applications.Models;
+using Bugger.Presentation.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bugger.Presentation.Test.Conveters
+{
+    [TestClass]
+    public class SettingDialogStatusToIndeterminateConverterTest
+    {
+        [TestMethod]
+        public void ConvertTest()
+        {
+            var converter = SettingDialogStatusToIndeterminateConverter.Default;
+
+            var result = (bool)converter.Convert(SettingDialogStatus.InitiatingProxyFailed, null, null, null);
+            Assert.IsFalse(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.InitiatingProxy, null, null, null);
+            Assert.IsTrue(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.NotWorking, null, null, null);
+            Assert.IsFalse(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.ProxyValid, null, null, null);
+            Assert.IsFalse(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.ValidatingProxySettings, null, null, null);
+            Assert.IsTrue(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.ProxyBusy, null, null, null);
+            Assert.IsFalse(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.ProxyCannotConnect, null, null, null);
+            Assert.IsFalse(result);
+
+            result = (bool)converter.Convert(SettingDialogStatus.ProxyUnvalid, null, null, null);
+            Assert.IsFalse(result);
+        }
+    }
+}
