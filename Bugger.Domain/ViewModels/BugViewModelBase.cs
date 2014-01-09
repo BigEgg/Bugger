b725b727925c39af7243d126f1f9ed7565122039
BigEgg_cp <SND\BigEgg_cp>
2014-01-09 08:13:09 -0800

    Add the abstract class for the base bug view model.

---
+++ Bugger.Domain/ViewModels/BugViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using BigEgg.Framework.Applications.ViewModels;
+using Bugger.Domain.Views;
+
+namespace Bugger.Domain.ViewModels
+{
+    /// <summary>
+    /// Abstract base class for a Bug ViewModel implementation.
+    /// </summary>
+    /// <typeparam name="TView">The type of the view. Do provide an interface as type and not the concrete type itself.</typeparam>
+    public abstract class BugViewModelBase<TView> : ViewModel<TView>
+        where TView : IBugView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BugViewModelBase{TView}"/> class.
+        /// And attaches itself as <c>DataContext</c> to the view.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        protected BugViewModelBase(TView view)
+            : base(view, true)
+        {
+
+        }
+    }
+}
---
+++ Bugger.Domain/Views/IBugView.cs
@@ -0,0 +1,11 @@
+﻿using BigEgg.Framework.Applications.Views;
+
+namespace Bugger.Domain.Views
+{
+    /// <summary>
+    /// Represents a view for the bug
+    /// </summary>
+    public interface IBugView : IView
+    {
+    }
+}
