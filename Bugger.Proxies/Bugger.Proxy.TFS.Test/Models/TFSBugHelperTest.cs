443e9b8a083ad7dc6eaca3e8d08789a5b98c88c6
BigEgg_cp <SND\BigEgg_cp>
2014-02-06 07:15:03 -0800

    Add the unit test for TFSBugHelper class.

---
+++ Bugger.Proxies/Bugger.Proxy.TFS.Test/Models/TFSBugHelperTest.cs
@@ -0,0 +1,32 @@
+﻿using Bugger.Proxy.TFS.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bugger.Proxy.TFS.Test.Models
+{
+    [TestClass]
+    public class TFSBugHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = TFSBugHelper.GetPropertyNames();
+
+            Assert.AreEqual(9, result.Count);
+
+            foreach (var model in result)
+            {
+                Assert.AreEqual(string.Empty, model.Value);
+            }
+
+            Assert.IsTrue(result.Keys.Contains("ID"));
+            Assert.IsTrue(result.Keys.Contains("Title"));
+            Assert.IsTrue(result.Keys.Contains("Description"));
+            Assert.IsTrue(result.Keys.Contains("AssignedTo"));
+            Assert.IsTrue(result.Keys.Contains("State"));
+            Assert.IsTrue(result.Keys.Contains("ChangedDate"));
+            Assert.IsTrue(result.Keys.Contains("CreatedBy"));
+            Assert.IsTrue(result.Keys.Contains("Priority"));
+            Assert.IsTrue(result.Keys.Contains("Severity"));
+        }
+    }
+}
