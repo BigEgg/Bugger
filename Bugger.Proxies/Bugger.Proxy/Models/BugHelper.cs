a4bab32c284c422b5d4b0c6b294e268ae4bff95e
BigEgg_cp <SND\BigEgg_cp>
2014-04-07 19:37:51 -0700

    Move the Helper and mapping Dictionary into proxy base.

---
+++ Bugger.Proxies/Bugger.Proxy/Models/BugHelper.cs
@@ -0,0 +1,47 @@
+﻿using Bugger.Proxy.Models.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Bugger.Proxy.Models
+{
+    public class BugHelper
+    {
+        private static IList<string> propertyNamesCache;
+
+
+        /// <summary>
+        /// Gets the new property mapping dictionary of the ITFSBug interface.
+        /// </summary>
+        /// <param name="bug">The bug interface.</param>
+        /// <returns>The new property mapping dictionary.</returns>
+        public static PropertyMappingDictionary GetPropertyNames()
+        {
+            GetNamesIfNotGet();
+
+            var result = new PropertyMappingDictionary();
+            foreach (var name in propertyNamesCache)
+            {
+                result.Add(name, string.Empty);
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Gets the property names if not get.
+        /// </summary>
+        private static void GetNamesIfNotGet()
+        {
+            if (propertyNamesCache == null)
+            {
+                IgnoreMappingAttribute ignore = new IgnoreMappingAttribute() { Ignore = true };
+                propertyNamesCache = TypeDescriptor.GetProperties(typeof(Bug))
+                                                   .Cast<PropertyDescriptor>()
+                                                   .Where(propertyInfo => !propertyInfo.Attributes.Contains(ignore))
+                                                   .Select(x => x.Name)
+                                                   .ToList();
+            }
+        }
+    }
+}
