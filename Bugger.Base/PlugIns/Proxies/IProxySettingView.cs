2aeac594993cd78dd577cc7956a7228cbf5d00ac
BigEgg <cd_bigegg@hotmail.com>
2015-01-09 17:10:40 +0800

    Add some interface for tracing system.

---
+++ Bugger.Base/PlugIns/Proxies/IProxySettingView.cs
@@ -0,0 +1,8 @@
+﻿using BigEgg.Framework.Applications.Views;
+
+namespace Bugger.Base.PlugIns.Proxies
+{
+    public interface IProxySettingView : IView
+    {
+    }
+}
---
+++ Bugger.Base/PlugIns/Proxies/ITracingSystemProxy.cs
@@ -0,0 +1,31 @@
+﻿using Bugger.Base.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Bugger.Base.PlugIns.Proxies
+{
+    public interface ITracingSystemProxy
+    {
+        #region Proxy Methods
+        /// <summary>
+        /// Query the bugs with the specified user name which the bug assign to.
+        /// </summary>
+        /// <param name="userName">The user name which the bug assign to.</param>
+        /// <param name="isFilterCreatedBy">if set to <c>true</c> indicating whether filter the created by field.</param>
+        /// <returns>
+        /// The bugs.
+        /// </returns>
+        ReadOnlyCollection<Bug> Query(string userName, bool isFilterCreatedBy = true);
+
+        /// <summary>
+        /// Query the bugs with the specified team members name list which the bug assign to.
+        /// </summary>
+        /// <param name="teamMembers">The team members name list which the bug assign to.</param>
+        /// <param name="isFilterCreatedBy">if set to <c>true</c> indicating whether filter the created by field.</param>
+        /// <returns>
+        /// The bugs.
+        /// </returns>
+        ReadOnlyCollection<Bug> Query(List<string> teamMembers, bool isFilterCreatedBy = false);
+        #endregion
+    }
+}
