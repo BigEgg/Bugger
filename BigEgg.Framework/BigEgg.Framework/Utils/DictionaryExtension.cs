66d90cb2455408355ae620f0d19480200a2d4970
BigEgg <cd_bigegg@hotmail.com>
2015-02-16 11:48:56 +0800

    Add basic framework project for some helper classes. 

---
+++ BigEgg.Framework/BigEgg.Framework/Utils/DictionaryExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace BigEgg.Framework.Utils
+{
+    public static class DictionaryExtension
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> list, TKey key, TValue value)
+        {
+            if (!list.ContainsKey(key))
+            {
+                list.Add(key, value);
+            }
+            else
+            {
+                list[key] = value;
+            }
+        }
+
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, IList<TValue>> list, TKey key, TValue value)
+        {
+            if (!list.ContainsKey(key))
+            {
+                list.Add(key, new List<TValue>() { value });
+            }
+            else
+            {
+                list[key].Add(value);
+            }
+        }
+    }
+}
