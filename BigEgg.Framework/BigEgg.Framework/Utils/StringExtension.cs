dc1ab7156b53a163a7ed595e81c460c6c5a31394
BigEgg <cd_bigegg@hotmail.com>
2015-02-17 23:20:52 +0800

    Add an extension method to create a string with multiple character.

---
+++ BigEgg.Framework/BigEgg.Framework/Utils/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigEgg.Framework.Utils
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Create a string with multiple character.
+        /// </summary>
+        /// <param name="character">The specific character.</param>
+        /// <param name="count">The time of the character in the string.</param>
+        /// <returns>The string.</returns>
+        public static string Build(this char character, int count)
+        {
+            StringBuilder builder = new StringBuilder(count);
+            for (int i = 0; i < count; i++)
+            {
+                builder.Append(character);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Framwork/BigEgg.Framework.Test/Utils/StringExtensionTest.cs
@@ -0,0 +1,18 @@
+﻿using BigEgg.Framework.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BigEgg.Framework.Test.Utils
+{
+    [TestClass]
+    public class StringExtensionTest
+    {
+        [TestMethod]
+        public void BuildTest()
+        {
+            var str = 'A'.Build(64);
+            Assert.AreEqual(64, str.Length);
+            Assert.AreEqual(64, str.Count(x => x == 'A'));
+        }
+    }
+}
