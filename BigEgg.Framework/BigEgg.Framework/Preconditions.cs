6a34e334b18dbb2031c5addecfbd922b21d209fd
BigEgg <cd_bigegg@hotmail.com>
2015-02-17 19:39:19 +0800

    Add a set of precondition logic to check the parameter.

---
+++ BigEgg.Framework/BigEgg.Framework/Preconditions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigEgg.Framework
+{
+    /// <summary>
+    /// Hold all the precondition logics.
+    /// </summary>
+    public class Preconditions
+    {
+        /// <summary>
+        /// Throw an <see cref="ArgumentNullException"> if the object is null.
+        /// </summary>
+        /// <param name="obj">The object which need to check.</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public static void NotNull(object obj, string paramName = "")
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+
+        /// <summary>
+        /// Throw an <see cref="ArgumentNullException"> if a specified string is null, 
+        /// empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The string to test</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public static void NotNullOrWhiteSpace(string value, string paramName = "")
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
---
+++ Framwork/BigEgg.Framework.Test/PreconditionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BigEgg.Framework.Test
+{
+    [TestClass]
+    public class PreconditionTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotNullTest_Null()
+        {
+            Preconditions.NotNull(null);
+        }
+
+        [TestMethod]
+        public void NotNullTest_NotNull()
+        {
+            Preconditions.NotNull("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotNullOrWhiteSpaceTest_Null()
+        {
+            Preconditions.NotNullOrWhiteSpace(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotNullOrWhiteSpaceTest_Empty()
+        {
+            Preconditions.NotNullOrWhiteSpace("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotNullOrWhiteSpaceTest_WhiteSpace()
+        {
+            Preconditions.NotNullOrWhiteSpace("  ");
+        }
+    }
+}
