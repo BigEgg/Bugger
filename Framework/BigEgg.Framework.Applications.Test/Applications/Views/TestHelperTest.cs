63514fc5040cc83616e8ad1c59cf856c3aa2dab3
BigEgg <cd_bigegg@hotmail.com>
2015-06-12 19:21:53 +0800

    Add view helper to get the view model by view

---
+++ Framework/BigEgg.Framework.Applications.Test/Applications/Views/TestHelperTest.cs
@@ -0,0 +1,49 @@
+﻿using BigEgg.Framework.Applications.Applications.ViewModels;
+using BigEgg.Framework.Applications.Applications.Views;
+using BigEgg.Framework.Applications.UnitTesting.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace BigEgg.Framework.Applications.Test.Applications.Views
+{
+    [TestClass]
+    public class ViewHelperTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetViewModelTest_ViewNull()
+        {
+            ViewHelper.GetViewModel(null);
+        }
+
+        [TestMethod]
+        public void GetViewModelTest()
+        {
+            IView view = new MockView();
+            var viewModel = new MockViewModel(view);
+
+            Assert.AreEqual(viewModel, view.GetViewModel<MockViewModel>());
+        }
+
+        [TestMethod]
+        public void GetViewModelWithDispatcherTest()
+        {
+            SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext());
+
+            var view = new MockView();
+            var viewModel = new MockViewModel(view);
+
+            Assert.AreEqual(viewModel, view.GetViewModel<MockViewModel>());
+        }
+
+
+        private class MockViewModel : ViewModel
+        {
+            public MockViewModel(IView view)
+                : base(view)
+            { }
+        }
+    }
+}
---
+++ Framework/BigEgg.Framework.Applications/Applications/Views/ViewHelper.cs
@@ -0,0 +1,46 @@
+﻿using BigEgg.Framework.Applications.Applications.ViewModels;
+using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace BigEgg.Framework.Applications.Applications.Views
+{
+    /// <summary>
+    /// Provides helper methods that perform common tasks involving a view.
+    /// </summary>
+    public static class ViewHelper
+    {
+        /// <summary>
+        /// Gets the ViewModel which is associated with the specified view.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        /// <returns>The associated ViewModel, or <c>null</c> when no ViewModel was found.</returns>
+        /// <exception cref="ArgumentNullException">view must not be <c>null</c>.</exception>
+        public static ViewModel GetViewModel(this IView view)
+        {
+            if (view == null) { throw new ArgumentNullException(); }
+
+            object dataContent = view.DataContext;
+            // When the DataContext is null then it might be that the ViewModel hasn't set it yet.
+            // Enforce it by executing the event queue of the Dispatcher.
+            if (dataContent== null && SynchronizationContext.Current is DispatcherSynchronizationContext)
+            {
+                DispatcherHelper.DoEvents();
+                dataContent = view.DataContext;
+            }
+            return dataContent as ViewModel;
+        }
+
+        /// <summary>
+        /// Gets the ViewModel which is associated with the specified view.
+        /// </summary>
+        /// <typeparam name="T">The type of the ViewModel</typeparam>
+        /// <param name="view">The view.</param>
+        /// <returns>The associated ViewModel, or <c>null</c> when no ViewModel was found.</returns>
+        /// <exception cref="ArgumentNullException">view must not be <c>null</c>.</exception>
+        public static T GetViewModel<T>(this IView view) where T : ViewModel
+        {
+            return GetViewModel(view) as T;
+        }
+    }
+}
