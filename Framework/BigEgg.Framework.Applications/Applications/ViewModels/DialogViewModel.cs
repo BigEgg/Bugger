218ba97df6f73d24c4521b04ade61058d9cf50e3
BigEgg <cd_bigegg@hotmail.com>
2015-06-12 18:51:31 +0800

    Add dialog view model and it's test

---
+++ Framework/BigEgg.Framework.Applications/Applications/ViewModels/DialogViewModel.cs
@@ -0,0 +1,48 @@
+﻿using BigEgg.Framework.Applications.Applications.Views;
+
+namespace BigEgg.Framework.Applications.Applications.ViewModels
+{
+    /// <summary>
+    /// Abstract base class for a DialogViewModel implementation.
+    /// </summary>
+    public abstract class DialogViewModel<TView> : ViewModel<TView>
+        where TView : IDialogView
+    {
+        /// <summary>
+        /// The dialog result
+        /// </summary>
+        protected bool? dialogResult;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogViewModel{TView}"/> class and
+        /// attaches itself as <c>DataContext</c> to the view.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        public DialogViewModel(TView view)
+            : base(view)
+        { }
+
+
+        /// <summary>
+        /// Show the dialog with an owner.
+        /// </summary>
+        /// <param name="owner">The owner of the dialog.</param>
+        /// <returns>Return the dialog result.</returns>
+        public virtual bool? ShowDialog(object owner)
+        {
+            ViewCore.ShowDialog(owner);
+            return this.dialogResult;
+        }
+
+
+        /// <summary>
+        /// Close the dialog with a result.
+        /// </summary>
+        /// <param name="dialogResult">The dialog result.</param>
+        public virtual void Close(bool? dialogResult)
+        {
+            this.dialogResult = dialogResult;
+            ViewCore.Close();
+        }
+    }
+}
---
+++ Framework/BigEgg.Framework.Applications/Applications/Views/IDialogView.cs
@@ -0,0 +1,18 @@
+﻿namespace BigEgg.Framework.Applications.Applications.Views
+{
+    /// <summary>
+    /// Represents a dialog view
+    /// </summary>
+    public interface IDialogView : IView
+    {
+        /// <summary>
+        /// Show the dialog view with an owner.
+        /// </summary>
+        void ShowDialog(object owner);
+
+        /// <summary>
+        /// Close the dialog view.
+        /// </summary>
+        void Close();
+    }
+}
