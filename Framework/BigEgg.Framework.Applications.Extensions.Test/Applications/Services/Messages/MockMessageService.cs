41b16919e75ace5fbb4e1be672888c628d0c5377
BigEgg <cd_bigegg@hotmail.com>
2015-07-24 00:01:36 +0800

    Add message service and its related classes in application extension project 

---
+++ Framework/BigEgg.Framework.Applications.Extensions.Test/Applications/Services/Messages/MockMessageService.cs
@@ -0,0 +1,61 @@
+﻿using BigEgg.Framework.Applications.Extensions.Applications.Services.Messages;
+using System;
+
+namespace BigEgg.Framework.Applications.Extensions.Test.Applications.Services.Messages
+{
+    public class MockMessageService : IMessageService
+    {
+        public MessageType MessageType { get; private set; }
+
+        public object Owner { get; private set; }
+
+        public string Message { get; private set; }
+
+        public Func<string, bool?> ShowQuestionAction { get; set; }
+
+        public Func<string, bool> ShowYesNoQuestionAction { get; set; }
+
+
+        public void ShowMessage(object owner, string message)
+        {
+            MessageType = MessageType.Message;
+            Owner = owner;
+            Message = message;
+        }
+
+        public void ShowWarning(object owner, string message)
+        {
+            MessageType = MessageType.Warning;
+            Owner = owner;
+            Message = message;
+        }
+
+        public void ShowError(object owner, string message)
+        {
+            MessageType = MessageType.Error;
+            Owner = owner;
+            Message = message;
+        }
+
+        public bool? ShowQuestion(object owner, string message)
+        {
+            Owner = owner;
+            return ShowQuestionAction(message);
+        }
+
+        public bool ShowYesNoQuestion(object owner, string message)
+        {
+            Owner = owner;
+            return ShowYesNoQuestionAction(message);
+        }
+    }
+
+
+    public enum MessageType
+    {
+        None,
+        Message,
+        Warning,
+        Error
+    }
+}
