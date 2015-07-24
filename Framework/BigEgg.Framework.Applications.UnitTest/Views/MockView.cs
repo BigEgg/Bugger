dbcb144a355e7928cc269d4eecd31d3832b9eba5
BigEgg <cd_bigegg@hotmail.com>
2015-07-24 15:49:45 +0800

    Remove the framework projects in local, just reference the core library

--- Framework/BigEgg.Framework.Applications.UnitTest/Views/MockView.cs
+++
@@ -1,9 +0,0 @@
-﻿using BigEgg.Framework.Applications.Applications.Views;
-
-namespace BigEgg.Framework.Applications.UnitTesting.Views
-{
-    public class MockView : IView
-    {
-        public object DataContext { get; set; }
-    }
-}
