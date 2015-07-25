018a3497d6f280d9f28cbd8f04a67388ad4a6d85
BigEgg <cd_bigegg@hotmail.com>
2015-07-25 21:17:33 +0800

    Update the project files and add the PlugIn Author class

---
+++ Bugger.Base.Test/PlugIns/Configs/PlugInAuthorTest.cs
@@ -0,0 +1,40 @@
+﻿using BigEgg.Framework.Utils;
+using Bugger.Base.PlugIns.Configs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+
+namespace Bugger.Domain.Test.PlugIns.Configs
+{
+    [TestClass]
+    public class PlugInAuthorTest
+    {
+        [TestMethod]
+        public void SerializeTest()
+        {
+            var author = new PlugInAuthor()
+            {
+                Name = "BigEgg",
+                EmailAddress = "bigegg@bigegg.com"
+            };
+            var xml = author.ObjectToXElement();
+
+            Assert.AreEqual("Author", xml.Name);
+            Assert.AreEqual("BigEgg", xml.Attribute("Name").Value);
+            Assert.AreEqual("bigegg@bigegg.com", xml.Value);
+        }
+
+        [TestMethod]
+        public void DeserializeTest()
+        {
+            var xml = new XElement("Author",
+                new XAttribute("Name", "BigEgg"),
+                new XText("bigegg@bigegg.com")
+            );
+            var author = xml.XElementToObject<PlugInAuthor>();
+
+            Assert.IsNotNull(author);
+            Assert.AreEqual("BigEgg", author.Name);
+            Assert.AreEqual("bigegg@bigegg.com", author.EmailAddress);
+        }
+    }
+}
---
+++ Bugger.Base/PlugIns/Configs/PlugInAuthor.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace Bugger.Base.PlugIns.Configs
+{
+    /// <summary>
+    /// The Plug-in's author's basic information.
+    /// </summary>
+    [XmlRoot("Author")]
+    public class PlugInAuthor
+    {
+        /// <summary>
+        /// Gets or sets the author name of the Plug-in.
+        /// </summary>
+        /// <value>
+        /// The author name of the Plug-in.
+        /// </value>
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address of the author.
+        /// </summary>
+        /// <value>
+        /// The email address of the author.
+        /// </value>
+        [XmlText]
+        public string EmailAddress { get; set; }
+    }
+}
