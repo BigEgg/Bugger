bb1f82bf1a93654eabff7daa2ef9d1126dd676ed
BigEgg <cd_bigegg@hotmail.com>
2015-08-18 21:41:25 +0800

    Add the strong type Bug class for Fake tracking system plugin

---
+++ Bugger.PlugIns.TrackingSystems/Bugger.PlugIns.TrackingSystems.Fake/Models/FakeBug.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bugger.PlugIns.TrackingSystems.Fake.Models
+{
+    public class FakeBug
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AssignedTo { get; set; }
+        public FakeBugState State { get; set; }
+        public DateTime LastChangedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public FakeBugPriority Priority { get; set; }
+        public FakeBugSeverity Severity { get; set; }
+    }
+
+    public enum FakeBugState
+    {
+        Design,
+        Implement,
+        Resolve,
+        Closed
+    }
+
+    public enum FakeBugPriority
+    {
+        High,
+        Medium,
+        Low
+    }
+
+    public enum FakeBugSeverity
+    {
+        High,
+        Medium,
+        Low
+    }
+}
