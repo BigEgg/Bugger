abaa3302005e6ff42529fdbeb3e9ab1993729431
BigEgg <cd_bigegg@hotmail.com>
2016-01-10 21:42:41 +0800

    Add extension methods to convert FakeBug to Bug class

---
+++ Bugger.PlugIns.TrackingSystems/Bugger.PlugIns.TrackingSystems.Fake.Test/Models/FakeBugExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using Bugger.PlugIns.TrackingSystems.Fake.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bugger.PlugIns.TrackingSystems.Fake.Test.Models
+{
+    [TestClass]
+    public class FakeBugExtensionsTest
+    {
+        [TestMethod]
+        public void ToBugTest()
+        {
+            var changedDate = DateTime.Now;
+
+            var fakeBug = new FakeBug()
+            {
+                Id = 1,
+                Title = "Some thing failed",
+                Description = "How to reproduce it",
+                AssignedTo = "BigEgg",
+                State = FakeBugState.Implement,
+                LastChangedDate = changedDate,
+                CreatedBy = "BigEgg",
+                Priority = FakeBugPriority.High,
+                Severity = FakeBugSeverity.Low
+            };
+
+            var bug = fakeBug.ToBug();
+            Assert.AreEqual("1", bug.ID);
+            Assert.AreEqual("Some thing failed", bug.Title);
+            Assert.AreEqual("How to reproduce it", bug.Description);
+            Assert.AreEqual("BigEgg", bug.AssignedTo);
+            Assert.AreEqual("Implement", bug.State);
+            Assert.AreEqual(changedDate, bug.LastChangedDate);
+            Assert.AreEqual("BigEgg", bug.CreatedBy);
+            Assert.AreEqual("High", bug.Priority);
+            Assert.AreEqual("Low", bug.Severity);
+        }
+    }
+}
---
+++ Bugger.PlugIns.TrackingSystems/Bugger.PlugIns.TrackingSystems.Fake/Models/FakeBugExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BigEgg.Framework;
+using Bugger.Models;
+
+namespace Bugger.PlugIns.TrackingSystems.Fake.Models
+{
+    public static class FakeBugExtensions
+    {
+        public static Bug ToBug(this FakeBug fakeBug)
+        {
+            Preconditions.NotNull(fakeBug);
+
+            return new Bug(
+                fakeBug.Id.ToString(),
+                fakeBug.Title,
+                fakeBug.Description,
+                fakeBug.AssignedTo,
+                fakeBug.State.ToString(),
+                fakeBug.LastChangedDate,
+                fakeBug.CreatedBy,
+                fakeBug.Priority.ToString(),
+                fakeBug.Severity.ToString());
+        }
+    }
+}
